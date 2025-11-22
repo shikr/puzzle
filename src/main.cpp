14fa86d9c6c364ebc1e3a082e7ba37a3d6dba132
shikr <cristobal.rod0907@gmail.com>
2025-11-21 22:07:30 -0600

    feat: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+int main() {
+  using namespace ftxui;
+
+  Element document = hbox({
+      text("left") | border,
+      text("middle") | border | flex,
+      text("right") | border,
+  });
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+
+  Render(screen, document);
+
+  screen.Print();
+}
