30a5bcf583912fad649666cd3ffc3d46f86b855f
shikr <cristobal.rod0907@gmail.com>
2025-11-22 20:23:30 -0600

    feat: add main page

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+#include <string>
+#include <vector>
+
+#include "ftxui/dom/elements.hpp"
+
+using namespace ftxui;
+
+const std::string LOGO =
+    " ██▓███   █    ██ ▒███████▒▒███████▒ ██▓    ▓█████ \n"
+    "▓██░  ██▒ ██  ▓██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▓██▒    ▓█   ▀ \n"
+    "▓██░ ██▓▒▓██  ▒██░░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░    ▒███   \n"
+    "▒██▄█▓▒ ▒▓▓█  ░██░  ▄▀▒   ░  ▄▀▒   ░▒██░    ▒▓█  ▄ \n"
+    "▒██▒ ░  ░▒▒█████▓ ▒███████▒▒███████▒░██████▒░▒████▒\n"
+    "▒▓▒░ ░  ░░▒▓▒ ▒ ▒ ░▒▒ ▓░▒░▒░▒▒ ▓░▒░▒░ ▒░▓  ░░░ ▒░ ░\n"
+    "░▒ ░     ░░▒░ ░ ░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒░ ░ ▒  ░ ░ ░  ░\n"
+    "░░        ░░░ ░ ░ ░ ░ ░ ░ ░░ ░ ░ ░ ░  ░ ░      ░   \n"
+    "            ░       ░ ░      ░ ░        ░  ░   ░  ░\n"
+    "                  ░        ░                       \n";
 
 int main() {
-  using namespace ftxui;
+  auto screen = ScreenInteractive::FullscreenAlternateScreen();
+  int selected = 0;
 
-  Element document = hbox({
-      text("left") | border,
-      text("middle") | border | flex,
-      text("right") | border,
+  std::vector<std::string> entries = {"Fácil", "Normal", "Difícil"};
+
+  auto buttons = Container::Horizontal(
+      {Button(
+           "Iniciar", [] {}, ButtonOption::Animated(Color::Green)) |
+           borderEmpty,
+       Button("Salir", screen.ExitLoopClosure(), ButtonOption::Animated(Color::Red)) |
+           borderEmpty});
+
+  MenuOption menuOption;
+  menuOption.on_enter = [&] { buttons->TakeFocus(); };
+  auto menu = Menu(&entries, &selected, menuOption);
+
+  auto layout = Container::Vertical({menu, buttons});
+
+  auto component = Renderer(layout, [&] {
+    return vbox({
+        filler(),
+        hbox({filler(),
+              vbox({paragraph(LOGO), menu->Render(),
+                    hbox({filler(), buttons->Render(), filler()})}),
+              filler()}),
+        filler(),
+    });
   });
 
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-
-  Render(screen, document);
-
-  screen.Print();
+  screen.Loop(component);
 }
